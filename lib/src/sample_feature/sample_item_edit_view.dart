f6a25a7a128f5c3de0477dcfb845cd3344f07db9
Choi Yeonung <39218496+yonsweng@users.noreply.github.com>
2025-01-28 20:31:09 +0900

    Add address to favorites (#3)
    Co-authored-by: Yeonung Choi <yeonung.choi@gausslabs.ai>

--- lib/src/sample_feature/sample_item_edit_view.dart
+++ lib/src/sample_feature/sample_item_edit_view.dart
@@ -1,5 +1,32 @@
 // Flutter imports:
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<String>> fetchData() async {
+  final headers = {
+    'x-ncp-apigw-api-key-id': dotenv.env['NAVER_MAP_CLIENT_ID']!,
+    'x-ncp-apigw-api-key': dotenv.env['NAVER_MAP_CLIENT_SECRET']!,
+    'Accept': 'application/json',
+  };
+
+  http.Response response = await http.get(
+      Uri.parse(
+          "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=테헤란로 201"),
+      headers: headers);
+
+  String jsonData = response.body;
+
+  // Parse the JSON data
+  var decodedData = jsonDecode(jsonData);
+  List<dynamic> addresses = decodedData['addresses'];
+  List<String> roadAddresses =
+      addresses.map((address) => address['roadAddress'] as String).toList();
+
+  return roadAddresses;
+}
 
 /// Displays the edit view for a SampleItem.
 class SampleItemEditView extends StatelessWidget {
@@ -13,8 +40,32 @@ class SampleItemEditView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Item Details'),
       ),
-      body: const Center(
-        child: Text('More Information Here'),
+      body: Center(
+        child: FutureBuilder<List<String>>(
+          future: fetchData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Text('No data found');
+            } else {
+              // Display the fetched road addresses in a ListView
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(snapshot.data![index]),
+                    onTap: () {
+                      Navigator.pop(context, snapshot.data![index]);
+                    },
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -1,100 +1,69 @@
-// Flutter imports:
 import 'package:flutter/material.dart';
-
-// Project imports:
-import '../settings/settings_view.dart';
-import 'sample_item.dart';
-import 'sample_item_details_view.dart';
 import 'sample_item_edit_view.dart';
+import 'sample_item_details_view.dart';
 
-/// Displays a list of SampleItems.
-class SampleItemListView extends StatelessWidget {
-  const SampleItemListView({
-    super.key,
-    this.items = const [SampleItem(1), SampleItem(2)],
-  });
+class SampleItemListView extends StatefulWidget {
+  const SampleItemListView({super.key});
 
-  static const routeName = '/';
+  static const routeName = '/sample_item_list';
 
-  final List<SampleItem> items;
+  @override
+  SampleItemListViewState createState() => SampleItemListViewState();
+}
+
+class SampleItemListViewState extends State<SampleItemListView> {
+  List<String?> items = List<String?>.filled(10, null);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('목적지를 선택하세요'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {
-              // Navigate to the settings page. If the user leaves and returns
-              // to the app after it has been killed while running in the
-              // background, the navigation stack is restored.
-              Navigator.restorablePushNamed(context, SettingsView.routeName);
-            },
-          ),
-        ],
+        title: const Text('Sample Item List'),
       ),
-
-      // To work with lists that may contain a large number of items, it’s best
-      // to use the ListView.builder constructor.
-      //
-      // In contrast to the default ListView constructor, which requires
-      // building all Widgets up front, the ListView.builder constructor lazily
-      // builds Widgets as they’re scrolled into view.
       body: ListView.builder(
-        // Providing a restorationId allows the ListView to restore the
-        // scroll position when a user leaves and returns to the app after it
-        // has been killed while running in the background.
         restorationId: 'sampleItemListView',
         itemCount: items.length,
         itemBuilder: (BuildContext context, int index) {
           final item = items[index];
 
-          if (index % 2 == 0) {
-            return ListTile(
-              title: Text('SampleItem ${item.id}'),
-              leading: const CircleAvatar(
-                // Display the Flutter Logo image asset.
-                foregroundImage: AssetImage('assets/images/flutter_logo.png'),
-              ),
-              onTap: () {
-                // Navigate to the details page. If the user leaves and returns to
-                // the app after it has been killed while running in the
-                // background, the navigation stack is restored.
-                Navigator.restorablePushNamed(
-                  context,
-                  SampleItemDetailsView.routeName,
-                );
-              },
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () {
-                  // Handle edit action here
-                  Navigator.restorablePushNamed(
-                    context,
-                    SampleItemEditView.routeName,
-                  );
-                },
-              ),
-            );
-          } else {
-            return ListTile(
-                title: const Text('+'),
-                leading: const CircleAvatar(
-                  // Display the Flutter Logo image asset.
-                  foregroundImage: AssetImage('assets/images/flutter_logo.png'),
-                ),
-                onTap: () {
-                  // Navigate to the details page. If the user leaves and returns to
-                  // the app after it has been killed while running in the
-                  // background, the navigation stack is restored.
-                  Navigator.restorablePushNamed(
-                    context,
-                    SampleItemDetailsView.routeName,
-                  );
+          return ListTile(
+            title: Text(item ?? '+'),
+            leading: const CircleAvatar(
+              // Display the Flutter Logo image asset.
+              foregroundImage: AssetImage('assets/images/flutter_logo.png'),
+            ),
+            onTap: () async {
+              // Navigate to the edit page and get the result.
+              final result = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const SampleItemEditView()),
+              );
+
+              if (result != null) {
+                setState(() {
+                  items[index] = result;
                 });
-          }
+              }
+            },
+            trailing: IconButton(
+              icon: const Icon(Icons.edit),
+              onPressed: () async {
+                // Handle edit action here
+                final result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SampleItemEditView()),
+                );
+
+                if (result != null) {
+                  setState(() {
+                    items[index] = result;
+                  });
+                }
+              },
+            ),
+          );
         },
       ),
     );
